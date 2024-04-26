36ad3834b2badc3952333baf33e68a28894e39d5
Srishty M <srishtymangutte@gmail.com>
2024-04-26 09:29:08 +0530

    feat: increased values in dataset and added testing data

--- createData.py
+++ createData.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-time_steps = np.arange(-10, +10, 0.01)
+time_steps = np.arange(-100, +101, 0.01)
 
 # create quadratic data
 y_coords = np.square(time_steps)
--- train.py
+++ train.py
@@ -1,5 +1,10 @@
 import pandas as pd
+import numpy as np
 from model import model as orbit_predictor
 
-orbit = pd.read_csv('./dataset/orbit.csv')
-orbit.head()
+orbit = pd.read_csv('./meteor_dataset.csv', index_col=0)
+print(orbit)
+
+# let's test againt last 1 minute of passing near earth
+
+x_test = np.arange(-10, 11, 0.01)
