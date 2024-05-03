7242af9048877d611dbac26316c9272b7d9f08cf
Srishty M <srishtymangutte@gmail.com>
2024-05-03 20:43:53 +0530

    fix: added layers

--- model.py
+++ model.py
@@ -1,15 +1,18 @@
 from keras.models import Sequential
 from keras.layers import Dense
 
-model = Sequential()
+model = Sequential([
+    Dense(10, input_shape=(1,), activation='relu'),
+    Dense(5, activation='relu'),
+    Dense(1)
+])
 
 #  input layer and a hidden layer with 10 neurons
-model.add(Dense(10, input_shape=(1,), activation="relu"))
-
-model.add(Dense(5, activation="relu"))
+#model.add(Dense(10, input_shape=(1,), activation="relu"))
+#model.add(Dense(5, activation="relu"))
 
 #  1-neuron output layer
-model.add(Dense(1))
+#model.add(Dense(1))
 
 
 # Compile your model
